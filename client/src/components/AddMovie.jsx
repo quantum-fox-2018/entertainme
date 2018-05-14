dfc597e11722e3d2acf20f3c563e6087e58210e1
haripermadi <harinurpermadi@gmail.com>
2018-05-14 22:44:00 +0700

    edit dikit

--- client/src/components/AddMovie.jsx
+++ client/src/components/AddMovie.jsx
@@ -19,7 +19,7 @@ class AddMovie extends Component {
   render() {
     let title, overview, poster_path, popularity, status
     return (
-      <div>
+      <div className="container">
         <h1>Add new movie</h1>
         <hr/>
         <Mutation mutation={ADD_MOVIE}>
@@ -34,14 +34,31 @@ class AddMovie extends Component {
               poster_path.value = ""
               popularity.value =""
               status.value = ""
+              this.props.history.push('/')
             }}
             >
-              <input placeholder="title" ref={node => { title = node; }}/>
-              <input placeholder="overview" ref={node => { overview = node; }}/>
-              <input placeholder="poster path.." ref={node => { poster_path = node; }}/>
-              <input placeholder="1" ref={node => { popularity = node; }}/>
-              <input placeholder="all ages" ref={node => { status = node; }}/>
-              <button type="submit">Submit</button>
+            <div class="form-group">
+              <label>Title</label>
+              <input class="form-control" placeholder="title" ref={node => { title = node; }}/>
+            </div>
+            <div class="form-group">
+              <label>Overview</label>
+              {/* <input class="form-control" placeholder="overview" ref={node => { overview = node; }}/> */}
+              <textarea class="form-control" rows="3" ref={node => { overview = node; }}></textarea>
+            </div>
+            <div class="form-group">
+              <label>Poster Path</label>
+              <input class="form-control" placeholder="poster path.." ref={node => { poster_path = node; }}/>
+            </div>
+            <div class="form-group">
+              <label>Popularity</label>
+              <input class="form-control" placeholder="1" ref={node => { popularity = node; }}/>
+            </div>
+            <div class="form-group">
+              <label>Status</label>
+              <input class="form-control" placeholder="all ages" ref={node => { status = node; }}/>
+            </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
           </div>
         )}
